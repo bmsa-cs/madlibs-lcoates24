4e54b31cbc0ee232d91c7e7e2177c9c06be6dbdf
Lauren Coates <lcoates24@biomedscienceacademy.org>
2020-11-10 14:37:42 +0000

    I added all my code

--- main.py
+++ main.py
@@ -5,5 +5,37 @@ Period/Core: 4
 
 
 """
+#This asks to play a game of silly sentences
+print("Let us play a game of Silly Sentences!")
 
-print("hello")
+#this is defining all the variables
+name = input("Pick a name: ")
+place = input("Pick a place: ")
+adjective1 = input("Pick an adjective: ")
+food1 = input("Pick a food: ")
+food2 = input("Pick another food: ")
+adjective2 = input("Pick another adjective: ")
+verb1 = input("Pick a past-tense verb: ")
+adjective3 = input("Pick another adjective: ")
+adjective4 = input("Pick another adjective: ")
+verb2 = input("Pick another verb: ")
+noun = input("Pick a noun: ")
+adverb1 = input("Pick an adverb: ")
+adjective5 = input("Pick another adjective: ")
+
+
+#This is the code that will print the story
+print(f"\n \n \n  {name} was getting ready for Thanksgiving at/in {place}.")
+print(f"{name} was excited, they could finally see their {adjective1}")
+print("family members!" + name + "got the foods they were bringing ready,")
+print(f"{food1} and {food2}.")
+print(f"\n  {name} remebered they were forgetting one {adjective2} thing")
+print("in the house!")
+print(f"\n  They {verb1} back inside and grabbed the {adjective3} shoes")
+print("off the kitchen table. Everyone always brought their own")
+print(f"pair of {adjective4} shoes to Thanksgiving! It was a")
+print(f"tradition to {verb2} them after Thanksgiving dinner.")
+print(f"\n  When {name} got to {place} something was wrong. Their")
+print(f"{noun} was missing! {name} improvised and made {noun} out of")
+print(f"stones and shoelaces. They {adverb1} walked inside {place}")
+print(f"for a {adjective5} Thanksgiving feast!")
